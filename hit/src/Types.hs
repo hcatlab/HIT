bb502b8604607a3f2746a772651805816fa32369
Vikraman Choudhury <git@vikraman.org>
2025-12-17 21:04:16 +0000

    feat: add JSON instances for all the types

--- hit/src/Types.hs
+++ hit/src/Types.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 
 module Types where
 
-import Data.Aeson (FromJSON (..), ToJSON (..))
+import Control.Applicative ((<|>))
+import Data.Aeson (FromJSON (..), ToJSON (..), defaultOptions, genericParseJSON)
+import Data.Char (toLower)
 import Data.Kind
 import Data.Text (Text)
 import Data.UUID (UUID)
@@ -20,7 +23,7 @@ data Fraction = Fraction
 instance ToJSON Fraction where
   toJSON (Fraction n d) = toJSON (show n ++ "/" ++ show d)
 
-instance FromJSON Fraction where  
+instance FromJSON Fraction where
   parseJSON v = do
     s <- parseJSON v
     let (numStr, denomStr) = break (== '/') s
@@ -28,7 +31,19 @@ instance FromJSON Fraction where
     let d = read (tail denomStr) :: Int
     return $ Fraction n d
 
-data Interval = Daily | Weekly deriving (Show, Eq, Generic)
+data Interval = Daily | Weekly
+  deriving (Show, Eq, Generic)
+
+instance ToJSON Interval where
+  toJSON = toJSON . map toLower . show
+
+instance FromJSON Interval where
+  parseJSON v = do
+    s <- parseJSON v
+    case map toLower s of
+      "daily" -> return Daily
+      "weekly" -> return Weekly
+      _ -> fail "Invalid Interval"
 
 data Weekdays = Weekdays
   { monday :: [Int],
@@ -39,42 +54,83 @@ data Weekdays = Weekdays
     saturday :: [Int],
     sunday :: [Int]
   }
-  deriving (Show, Eq, Generic)
+  deriving (Show, Eq, Generic, ToJSON, FromJSON)
 
 data Deadline :: Interval -> Type where
   DayHours :: [Int] -> Deadline Daily
   WeekHours :: Weekdays -> Deadline Weekly
 
-instance Show (Deadline p) where
-  show (DayHours ts) = show ts
-  show (WeekHours ws) = show ws
+deriving instance Show (Deadline p)
 
-instance Eq (Deadline p) where
-  (DayHours ts) == (DayHours ss) = ts == ss
-  (WeekHours ws) == (WeekHours vs) = ws == vs
+deriving instance Eq (Deadline p)
 
-data Habit a p = Habit
+instance ToJSON (Deadline p) where
+  toJSON (DayHours hrs) = toJSON hrs
+  toJSON (WeekHours wds) = toJSON wds
+
+instance FromJSON (Deadline Daily) where
+  parseJSON v = DayHours <$> parseJSON v
+
+instance FromJSON (Deadline Weekly) where
+  parseJSON v = WeekHours <$> parseJSON v
+
+data Sort = YesNo Bool | Times Int
+  deriving (Show, Eq, Generic)
+
+instance ToJSON Sort where
+  toJSON (YesNo b) = toJSON b
+  toJSON (Times n) = toJSON n
+
+instance FromJSON Sort where
+  parseJSON v = (YesNo <$> parseJSON v) <|> (Times <$> parseJSON v)
+
+data Habit p = Habit
   { id :: UUID,
     name :: Text,
-    kind :: a,
+    sort :: Sort,
     description :: Maybe Text,
     rate :: Fraction,
     deadline :: Deadline p
   }
-  deriving (Show, Eq, Generic)
+  deriving (Show, Eq, Generic, ToJSON)
 
-data Intention p = Intention 
+instance FromJSON (Habit Daily) where
+  parseJSON = genericParseJSON defaultOptions
+
+instance FromJSON (Habit Weekly) where
+  parseJSON = genericParseJSON defaultOptions
+
+data Intention p = Intention
   { id :: UUID,
     name :: Text,
     description :: Maybe Text,
     rate :: Fraction,
     deadline :: Deadline p
   }
-  deriving (Show, Eq, Generic)
+  deriving (Show, Eq, Generic, ToJSON)
 
-data State =
-    Pending
+instance FromJSON (Intention Daily) where
+  parseJSON = genericParseJSON defaultOptions
+
+instance FromJSON (Intention Weekly) where
+  parseJSON = genericParseJSON defaultOptions
+
+data State
+  = Pending
   | InProgress
   | Done
   | Urgent
   deriving (Show, Eq, Generic)
+
+instance ToJSON State where
+  toJSON = toJSON . map toLower . show
+
+instance FromJSON State where
+  parseJSON v = do
+    s <- parseJSON v
+    case map toLower s of
+      "pending" -> return Pending
+      "inprogress" -> return InProgress
+      "done" -> return Done
+      "urgent" -> return Urgent
+      _ -> fail "Invalid State"
